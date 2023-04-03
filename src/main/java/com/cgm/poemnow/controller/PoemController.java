16e6ca8fea46c19cb7c1abfadcde40a55293a304
andbeyond0331 <mjpark1329@naver.com>
2023-04-03 11:31:53 +0900

    refactor: 명확한 정의를 위해 MainController -> PoemController 이름 변경

--- src/main/java/com/cgm/poemnow/controller/PoemController.java
+++ src/main/java/com/cgm/poemnow/controller/PoemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class PoemController {
 	@Autowired
 	private PoemService poemService;
 
