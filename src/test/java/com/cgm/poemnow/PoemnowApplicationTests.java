3a219d7bcc821cbd668b2dfb131255d72df1d88e
andbeyond0331 <mjpark1329@naver.com>
2023-04-04 20:57:39 +0900

    feat: PoemnowApplicationTests.java 추가 - getPoemList() method 작성

--- src/test/java/com/cgm/poemnow/PoemnowApplicationTests.java
+++ src/test/java/com/cgm/poemnow/PoemnowApplicationTests.java
@@ -1,13 +1,25 @@
 package com.cgm.poemnow;
 
+import com.cgm.poemnow.service.PoemServiceImpl;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PoemnowApplicationTests {
 
+	@Autowired
+	PoemServiceImpl poemService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getPoemList() {
+		String result = poemService.listPoem().toString();
+		System.out.println("result : " + result);
 	}
 
 }
