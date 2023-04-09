a9cb57869088f07a7775b758f0ac4de7ad60309c
BaeYoungSuk <bysuk01@gmail.com>
2023-04-09 12:08:59 +0900

    feat: 시집, 시, 유저 기반 검색 로직 구현

---
+++ src/main/java/com/cgm/poemnow/controller/UserSearchController.java
@@ -0,0 +1,29 @@
+package com.cgm.poemnow.controller;
+
+import com.cgm.poemnow.domain.User;
+
+import com.cgm.poemnow.service.UserSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/userSearch")
+public class UserSearchController {
+    @Autowired
+    private UserSearchService userSearchService;
+
+    @GetMapping("/userSearchByNickname")
+    public ResponseEntity<?> poemSearchByTitle(
+            @RequestParam(value = "keyword") String keyword,
+            @RequestParam(value = "sortOrder", required = false, defaultValue = "lastest") String sortOrder
+    ){
+        List<User> response = userSearchService.findUsersByNickname(keyword, sortOrder);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+}
